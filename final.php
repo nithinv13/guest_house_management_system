5b575994df69fea8901dd70e617ab0d3304f813c
Upendra Singh Chauhan <upendrasingh729@gmail.com>
2016-04-02 23:51:47 +0530

    Next Version
    One page added

--- final.php
+++ final.php
@@ -24,13 +24,85 @@ $today = date("Y-m-d");
 		//$total_price=$_SESSION["num_of_rooms"]*$days*$prize;
 		//echo $days;
 		$obj= new billing;
-		echo $obj->Payment_calc($prize,$num_of_rooms,$days);
+		//echo $obj->Payment_calc($prize,$num_of_rooms,$days);
 		//echo $total_price;
 	
 ?>
 
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+    border-collapse: collapse;
+    width: 100%;
+}
 
+th, td {
+    text-align: left;
+    padding: 8px;
 
+}
 
-?>
+tr:nth-child(even){background-color: #f2f2f2}
+
+th {
+    background-color: #4CAF50;
+    color: white;
+}
+
+.button {
+    background-color: #4CAF50;
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+}
+</style>
+</head>
+<body>
+
+<h2>Order Details</h2>
+
+<table>
+  <tr>
+    <th>Total Ammount</th>
+    <th><?php echo $obj->Payment_calc($prize,$num_of_rooms,$days); ?></th>
+    
+  </tr>
+  <tr>
+    <td>Total Rooms</td>
+    <td><?php echo $num_of_rooms ?></td>
+  
+  </tr>
+  <tr>
+    <td>Total Guests</td>
+    <td><?php echo $_SESSION["num_of_guests"] ?></td>
+    
+  </tr>
+  <tr>
+    <td>Start Date</td>
+    <td><?php echo $_SESSION["start_date"];  ?></td>
+    
+  </tr>
+  <tr>
+    <td>End Date</td>
+    <td><?php echo $_SESSION["end_date"];  ?></td>
+    
+</tr>
+<tr>
+    <td>Number of Days</td>
+    <td><?php echo $days;  ?></td>
+    
+</tr>
+</table>
+
+<button class="button" type="button" onclick="window.location.href='requirements_registered.php'">Send Request</button>
+
+</body>
+</html>
